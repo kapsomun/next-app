d37f9570b9c99b2bb5f7177327a8cbc21556795e
kapsomun <mazajko@gmail.com>
2023-01-10 20:27:59 +0200

    added page with events in current city

--- app/events/[cat]/page.js
+++ app/events/[cat]/page.js
@@ -1,15 +1,18 @@
 
 import Link from 'next/link';
 import React from 'react'
-import {events_categories} from "../../utils/data.json"
+import {allEvents} from "../../../utils/data.json"
 
 
 
-function EventsPage() {
-    const events = events_categories;
+function EventsInCityPage({params}) {
+    const data = allEvents;
+    const id = params.cat;
+    const events = data.filter(e => e.city == id)
+    
   return (
     <div className='events'>
-       { events.map((e) => <Link href={`/events/${e.id}`} key={e.id}>
+       { events.map((e) => <Link href={e.id} key={e.id}>
         <div className="events__item">
             <img className='events__item_img' src={e.image} alt={e.id} />
             <h2>{e.title}</h2>
@@ -20,4 +23,4 @@ function EventsPage() {
   )
 }
 
-export default EventsPage
+export default EventsInCityPage
